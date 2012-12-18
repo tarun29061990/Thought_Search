4a18bbe1cec34257f24712bc941967ff85b95d05
tarun29061990 <tarun29061990@gmail.com>
2012-12-18 21:59:44 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+
+This is an extension of my previous application EMBER.JS REAL TIME TWEET SEARCH ENGINE https://github.com/tarun29061990/Tweet_Search.git ... 
+
+I was not fully satisfied with my previous application so I integrated a attitude feature in it and I named it as thought search. Suppose you want to search about a thought about say "robot" and you want to have these thoughts with a positive attitude like "This robot is awesome".. So in order to do this type the "robot" keyword in the textfield and assign a attitude "positive/negative/normal" and hit "GO!" and boom the result will be in front of your eyes... :)
+
+I have coded this application in Ember.js.
+
+
+Feel free to use it and take the most of it... :D :D
+
---
+++ app.js
@@ -0,0 +1,143 @@
+/* @copyright Tarun Chaudhary 
+B tech (EE)
+IIT Rajasthan
+tarun29061990@gmail.com
+
+tarun29061990@github*/
+
+/**************************
+* Application
+**************************/
+
+App=Em.Application.create({
+	
+});
+
+/**************************
+* Model
+**************************/
+App.Tweet=Em.Object.extend({
+	
+
+});
+
+App.Master =Ember.Object.extend({
+	me_url:null,
+	facebook_url:null
+});
+
+
+
+/**************************
+* View
+**************************/
+
+App.SearchTextField=Em.TextField.extend({
+	
+});
+var search=App.SearchTextField.create({
+	insertNewLine:function(){
+		App.SearchResultsController.refresh();
+	},
+
+	remove:function(){
+		this.remove();
+	}
+
+});
+
+App.TweetCountView=Ember.View.extend({
+	count:null
+});
+/**************************
+* Controller
+**************************/
+
+App.searchResultsController=Em.ArrayController.create({
+	content:[],
+	query:'',
+	_idCache: {},
+	tweet_count:'',
+	parameter:null,
+	
+	addTweet: function(tweet) {
+    // The `id` from Twitter's JSON
+    	var id = tweet.get("id");
+    	this.parameter++;
+    // If we don't already have an object with this id, add it.
+    	if (typeof this._idCache[id] === "undefined") {
+      		this.pushObject(tweet);
+      		this._idCache[id] = tweet.id;
+    	}
+
+    	if(this.parameter>2)
+    		{App.TimeInterval.bigInterval();}
+    	else
+    		App.TimeInterval.interval();
+ 	
+ 	},
+
+	reverse:function(){
+		return this.toArray().reverse();
+	}.property('@each'),
+
+	refresh:function(){
+		var self=this;
+		var query=self.get("query");
+			var url="http://search.twitter.com/search.json?q="+query+App.currentOption.value+"&callback=?"
+		
+			$.getJSON(url,function(data){
+					
+						
+						for (var i = 0; i < data.results.length; i++) 
+						{
+        					self.addTweet(App.Tweet.create(
+        						data.results[i]));
+						}
+      					
+			});
+		
+	}
+});
+
+App.TimeInterval=Ember.ArrayController.create({
+	interval:function(){
+		setInterval(function() {
+      	App.searchResultsController.refresh();
+    	}, 15000);
+	},
+
+	bigInterval:function(){
+		setInterval(function() {
+      	App.searchResultsController.refresh();
+    	}, 100000);
+	}
+});
+
+App.masterController=Ember.ArrayController.create({
+	content:[
+		App.Master.create({
+		me_url:"https://graph.facebook.com/tarun29061990/picture/",
+		facebook_url:"https://www.facebook.com/tarun29061990/",
+		})
+	]
+});
+
+App.selectedOptionController=Em.ArrayController.create({
+	content:[
+		Em.Object.create({name: 'normal', id:""}),
+		Em.Object.create({name:'positive', id:":)"}),
+		Em.Object.create({name:'negative', id:":("})
+	]
+});
+
+App.currentOption=Em.Object.create({
+	valueOption:"",
+	valueBinding:Em.Binding.oneWay('valueOption.id')
+})
+
+
+
+
+
+
